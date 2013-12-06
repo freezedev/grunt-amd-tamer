7736bcd472dbf11c8f1dc50bc78aa4617c4726b1
Johannes Stein <johannesstein@freeze-dev.com>
2013-12-06 18:50:06 +0100

    Updated expected

---
+++ test/expected/coffee.coffee
@@ -0,0 +1,3 @@
+define 'a', -> 5
+define 'b', ['a'], (a) -> a * 4
+define('c', 'c', -> 10)
---
+++ test/expected/javascript_simple.js
@@ -0,0 +1,19 @@
+define('folder', ['folder/submodule'], function() {
+  return 10;
+});
+
+define('folder/submodule', {
+  c: 3,
+  d: 5
+});
+
+define('module', ['a', 'b', 'c'], function() {
+  return 5;
+});
+define('named1', function() {
+  return "it's alive!";
+});
+
+define('named2', ['b'], function() {
+  return "it's alive!";
+});
