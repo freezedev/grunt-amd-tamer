da380e034df102454804ae1af116218c4fe2deb3
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-10 21:13:37 +0200

    Update fixture

--- test/expected/javascript_dots.js
+++ test/expected/javascript_dots.js
@@ -1,11 +1,35 @@
 define('a.b', function() {
-	return 'a.b';
+  return 'a.b';
 });
 
 define('c.d', function() {
-	return 'c.d';
+  return 'c.d';
 });
 
 define('e.f.g.h', function() {
-	return 'e.f.g.h';
+  return 'e.f.g.h';
+});
+
+define('folder/a.b', function() {
+  return 'a.b';
+});
+
+define('folder/c.d', function() {
+  return 'c.d';
+});
+
+define('folder/e.f.g.h', function() {
+  return 'e.f.g.h';
+});
+
+define('folder/subfolder/a.b', function() {
+  return 'a.b';
+});
+
+define('folder/subfolder/c.d', function() {
+  return 'c.d';
+});
+
+define('folder/subfolder/e.f.g.h', function() {
+  return 'e.f.g.h';
 });
